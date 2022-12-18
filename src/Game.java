524935688ba3efe519166515625c5696c78c1f25
NikaShabashvili03 <104011629+NikaShabashvili03@users.noreply.github.com>
2022-12-18 05:16:39 +0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -10,11 +10,12 @@ public class Game {
                     {"paper","spock"},
                     {"spock","rock"},
                     {"rock","scissors"},
+                    {"lizard","spock"},
             };
     Game(){};
     Game(String player1, String player2){
         String winner = "";
-        for(int i = 0; i <= 8; i++){
+        for(int i = 0; i <= 9; i++){
             if(player1 == arrs[i][0] && player2 == arrs[i][1]){
                 winner = "Player 1 Won!";
             }
